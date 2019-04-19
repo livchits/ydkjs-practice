1fb9c3732c7ab4c03c98dd5a3df560da0230c692
Lucas Livchits <livchitsl@gmail.com>
2019-04-19 16:18:54 -0300

    New version

--- purchase.js
+++ purchase.js
@@ -9,14 +9,14 @@
 
 var bankAccountBalance = prompt("¿Cuánto dinero tiene en su cuenta?");
 const SPENDING_THRESHOLD = bankAccountBalance * 0.4; 
-const TAX_RATE = 0.21;
-const PHONE_PRICE = 69.99;
-const ACCESSORY_PRICE = 19.99;
+const TAX_RATE = 0.1;
+const PHONE_PRICE = 100;
+const ACCESSORY_PRICE = 10;
 var total = 0;
 var phones = 0;
 var accessories = 0;
 
-function addTax(amount) {
+function tax(amount) {
   return amount * TAX_RATE;
 }
 
@@ -24,17 +24,16 @@ function format(amount) {
   return "$" + amount.toFixed(2);
 }
 
-while (bankAccountBalance >= addTax(PHONE_PRICE)) {
-  total += addTax(PHONE_PRICE);
-  bankAccountBalance -= total;
-  phones++
-  if ((total + addTax(ACCESSORY_PRICE)) < SPENDING_THRESHOLD) {
-    total += addTax(ACCESSORY_PRICE);
-    bankAccountBalance -= addTax(ACCESSORY_PRICE);
-    accessories++  
+while (bankAccountBalance > (PHONE_PRICE + tax(PHONE_PRICE)) && bankAccountBalance > SPENDING_THRESHOLD) {
+//Si en la cuenta hay más del valor de un teléfono con impuestos y más de lo que me puedo permitir gastar, se compra un teléfono.
+  total += PHONE_PRICE + tax(PHONE_PRICE);
+  phones++;
+  if ((total + ACCESSORY_PRICE + tax(ACCESSORY_PRICE)) < SPENDING_THRESHOLD) {
+  //Si lo que me puedo permitir gastar es más que el total sumado a un accesorio y sus impuestos, se compra un accesorio.
+    total += ACCESSORY_PRICE + tax(ACCESSORY_PRICE);
+    accessories++      
   }
+  bankAccountBalance -= total;
 }
 
 console.log(`El total de la compra es ${format(total)}.\nSe compraron ${phones} teléfonos y ${accessories} accesorios.`);
-
-
